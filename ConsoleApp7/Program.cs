5e657b6ababc5c180662933af3bae0c34a33a8e2
Mr. Wilson <bwilson@macon.k12.mo.us>
2020-12-17 09:04:46 -0600

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DateTime myDate = DateTime.Now;
+
+            //string myDay = myDate.DayOfWeek;
+
+            //string firstName = " Bob ";
+
+            //string lastName = " Tabor ";
+
+            //string ans = string.Format(" Hello {0}, is your last name {1} ?", firstName, lastName);
+
+            double ans = 50.00;
+
+                ans = ans * .07259;
+
+            string tax = string.Format(" Total Tax {0:C}", ans);
+
+            Console.WriteLine(tax);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
